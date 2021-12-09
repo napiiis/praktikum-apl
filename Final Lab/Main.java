3b474d056e4cd6e2e29d530a913f5907e402e896
napiiis <72500111+napiiis@users.noreply.github.com>
2021-12-09 10:34:14 +0700

    Update Main.java

--- Final Lab/Main.java
+++ Final Lab/Main.java
@@ -3,9 +3,9 @@ class Main {
          
         Observable admin = new Publisher(); //create new object dari class Publisher
         //create 3 object sebagai observer
-        Observer cust1 = new Customer("pis");
-        Observer cust2 = new Customer("co");
-        Observer cust3 = new Customer("ki");
+        Observer cust1 = new Customer("Muhammad Ikbal Firdausy");
+        Observer cust2 = new Customer("Mufti Ahmad");
+        Observer cust3 = new Customer("Mikail Rifqi");
 
         admin.addObserver(cust1);
         admin.addObserver(cust2);
@@ -16,7 +16,7 @@ class Main {
 }
 
 
-// Magazine Shop (Toko Majalah)
+// Magazine Store (Toko Majalah)
 // 
 // Customer akan mendapatkan notifikasi terkait majalah" terbaru yang diterbitkan dengan jenis majalahnya. 
 // Jenis majalah tersebut bisa berupa majalah olahraga/sports dan gaya hidup/lifestyle.
